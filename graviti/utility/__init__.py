51c63a332ecd8fc5d0b49b5bf37512a1176e9ee5
changjun.zhu <changjun.zhu@graviti.com>
2022-01-26 19:27:22 +0800

    feat(utility): add implementation about "open_api_do"
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/13

--- graviti/utility/__init__.py
+++ graviti/utility/__init__.py
@@ -3,3 +3,7 @@
 # Copyright 2022 Graviti. Licensed under MIT License.
 #
 """Utility module."""
+
+from graviti.utility.request import URL_PATH_PREFIX, open_api_do
+
+__all__ = ["open_api_do", "URL_PATH_PREFIX"]
---
+++ graviti/utility/request.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Graviti. Licensed under MIT License.
+#
+
+"""The basic concepts and methods of the Graviti OpenAPI."""
+
+from typing import Any
+from uuid import uuid4
+
+from requests.models import Response
+from tensorbay.exception import ResponseError, ResponseErrorDistributor
+from tensorbay.utility import config, get_session
+
+from graviti.__version__ import __version__
+
+URL_PATH_PREFIX = "gatewayv2/tensorbay-open-api/v1"
+
+
+def open_api_do(url: str, access_key: str, method: str, **kwargs: Any) -> Response:
+    """Send a request to the Graviti OpenAPI.
+
+    Arguments:
+        access_key: User's access key.
+        method: The method of the request.
+        url: The URL of the graviti website.
+        **kwargs: Extra keyword arguments to send in the POST request.
+
+    Raises:
+        ResponseError: When the status code OpenAPI returns is unexpected.
+
+    Returns:
+        Response of the request.
+
+    """
+    headers = kwargs.setdefault("headers", {})
+    headers["X-Token"] = access_key
+    headers["X-Source"] = f"{config._x_source}/{__version__}"  # pylint: disable=protected-access
+    headers["X-Request-Id"] = uuid4().hex
+
+    try:
+        return get_session().request(  # type: ignore[no-any-return]
+            method=method, url=url, **kwargs
+        )
+    except ResponseError as error:
+        response = error.response
+        error_code = response.json()["code"]
+        raise ResponseErrorDistributor.get(error_code, ResponseError)(response=response) from None
