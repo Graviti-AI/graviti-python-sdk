daaefc40fe29927601718e299f75b0b9ba4f96fc
changjun.zhu <changjun.zhu@graviti.com>
2022-03-21 17:45:53 +0800

    feat(dataset): design structure and interfaces of draft
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/62

--- graviti/manager/draft.py
+++ graviti/manager/draft.py
@@ -5,27 +5,27 @@
 
 """The implementation of the Draft and DraftManager."""
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Iterator, KeysView, MutableMapping, Optional
 
-from tensorbay.client.struct import User
-
+from graviti.dataframe import DataFrame
 from graviti.manager.lazy import PagingList
 
 if TYPE_CHECKING:
     from graviti.manager.dataset import Dataset
 
 
-class Draft:
-    """This class defines the basic structure of a draft.
+class Draft(MutableMapping[str, DataFrame]):
+    """The basic structure of the Graviti draft.
 
     Arguments:
         dataset: Class :class:`~graviti.dataset.dataset.Dataset` instance.
         number: The number of the draft.
         title: The title of the draft.
         branch_name: The branch name.
-        status: The draft status which includes "OPEN", "CLOSED", "COMMITTED".
+        state: The draft state which includes "OPEN", "CLOSED", "COMMITTED".
         parent_commit_id: The parent commit id.
-        author: The author of the draft.
+        creator: The creator of the draft.
+        created_at: The time when the draft is created.
         updated_at: The time of last update.
         description: The draft description.
 
@@ -38,13 +38,37 @@ class Draft:
         *,
         title: str,
         branch_name: str,
-        status: str,
+        state: str,
         parent_commit_id: str,
-        author: User,
-        updated_at: int,
+        creator: str,
+        created_at: str,
+        updated_at: str,
         description: str = "",
     ) -> None:
         pass
+
+    def __len__(self) -> int:
+        pass
+
+    def __getitem__(self, key: str) -> DataFrame:
+        pass
+
+    def __iter__(self) -> Iterator[str]:
+        pass
+
+    def __setitem__(self, key: str, value: DataFrame) -> None:
+        pass
+
+    def __delitem__(self, key: str) -> None:
+        pass
+
+    def keys(self) -> KeysView[str]:
+        """Return a new view of the keys in dict.
+
+        Return:
+            The keys in dict.
+
+        """
 
     def edit(self, title: Optional[str] = None, description: Optional[str] = None) -> None:
         """Update title and description of the draft.
@@ -57,6 +81,23 @@ class Draft:
 
     def close(self) -> None:
         """Close the draft."""
+
+    def commit(self, title: str, description: str = "") -> None:
+        """Commit the current draft.
+
+        Arguments:
+            title: The commit title.
+            description: The commit description.
+
+        """
+
+    def upload(self, jobs: int = 1) -> None:
+        """Upload the local dataset to Graviti.
+
+        Arguments:
+            jobs: The number of the max workers in multi-thread upload.
+
+        """
 
 
 class DraftManager:
@@ -95,12 +136,12 @@ class DraftManager:
         """
 
     def list(
-        self, status: Optional[str] = "OPEN", branch_name: Optional[str] = None
+        self, state: Optional[str] = "OPEN", branch_name: Optional[str] = None
     ) -> PagingList[Draft]:
         """List all the drafts.
 
         Arguments:
-            status: The draft status which includes "OPEN", "CLOSED", "COMMITTED", "ALL" and None.
+            state: The draft state which includes "OPEN", "CLOSED", "COMMITTED", "ALL" and None.
                     where None means listing open drafts.
             branch_name: The branch name.
 
