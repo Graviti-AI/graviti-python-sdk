bb07dfd7ab0cd39c614ad5206da9138b20e93298
yexuan.li <yexuan.li@graviti.com>
2022-03-25 00:58:21 +0800

    build: add setup.py and setup.cfg
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/68

--- graviti/__init__.py
+++ graviti/__init__.py
@@ -5,7 +5,8 @@
 
 """Graviti Python SDK."""
 
+from graviti.__version__ import __version__
 from graviti.dataframe import DataFrame
 from graviti.graviti import Graviti
 
-__all__ = ["DataFrame", "Graviti"]
+__all__ = ["__version__", "DataFrame", "Graviti"]
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Graviti. Licensed under MIT License.
+#
+
+"""Generic setup script."""
+
+import setuptools
+
+setuptools.setup()
