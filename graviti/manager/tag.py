9e822d2f30bc13df06ddeca2de42764b9853aacd
changjun.zhu <changjun.zhu@graviti.com>
2022-04-15 16:17:19 +0800

    feat(manager): add class "Tag" to support basic version control
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/101

--- graviti/manager/tag.py
+++ graviti/manager/tag.py
@@ -3,16 +3,32 @@
 # Copyright 2022 Graviti. Licensed under MIT License.
 #
 
-"""The implementation of the TagManager."""
+"""The implementation of the Tag and TagManager."""
 
 from typing import TYPE_CHECKING, Optional
 
-from tensorbay.client.struct import Tag
+from tensorbay.client.struct import Tag as TensorbayTag
 
+from graviti.manager.commit import NamedCommit
 from graviti.manager.lazy import PagingList
 
 if TYPE_CHECKING:
     from graviti.manager.dataset import Dataset
+
+
+class Tag(NamedCommit):
+    """This class defines the structure of the tag of a commit.
+
+    Arguments:
+        name: The name of the tag.
+        commit_id: The commit id.
+        parent_commit_id: The parent commit id.
+        title: The commit title.
+        description: The commit description.
+        committer: The commit user.
+        committed_at: The time when the draft is committed.
+
+    """
 
 
 class TagManager:
@@ -26,7 +42,7 @@ class TagManager:
     def __init__(self, dataset: "Dataset") -> None:
         pass
 
-    def create(self, name: str, revision: Optional[str] = None) -> Tag:
+    def create(self, name: str, revision: Optional[str] = None) -> TensorbayTag:
         """Create a tag for a commit.
 
         Arguments:
@@ -36,26 +52,26 @@ class TagManager:
                 If the revision is not given, create the tag for the current commit.
 
         Return:
-            The :class:`.Tag` instance with the given name.
+            The :class:`.TensorbayTag` instance with the given name.
 
         """
 
-    def get(self, name: str) -> Tag:
+    def get(self, name: str) -> TensorbayTag:
         """Get the certain tag with the given name.
 
         Arguments:
             name: The required tag name.
 
         Return:
-            The :class:`.Tag` instance with the given name.
+            The :class:`.TensorbayTag` instance with the given name.
 
         """
 
-    def list(self) -> PagingList[Tag]:
+    def list(self) -> PagingList[TensorbayTag]:
         """List the information of tags.
 
         Return:
-            The PagingList of :class:`tags<.Tag>` instances.
+            The PagingList of :class:`tags<.TensorbayTag>` instances.
 
         """
 
