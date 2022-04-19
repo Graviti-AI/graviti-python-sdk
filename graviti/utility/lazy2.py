c9880d1787ef4cb32782ba4800c0274ff7d6ce86
linji.xue <linji.xue@graviti.cn>
2022-04-19 18:13:58 +0800

    refactor(utility): set "LazyFactory" as data manager
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/105

---
+++ graviti/utility/lazy2.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Graviti. Licensed under MIT License.
+#
+
+"""Lazy list related class."""
+
+from itertools import chain
+from math import ceil
+from typing import Any, Callable, Iterator, List, Optional, Tuple, Union
+
+import pyarrow as pa
+
+
+class LazyList:
+    """LazyList is a lazy load list.
+
+    Arguments:
+        factory: The parent :class:`LazyFactory` instance.
+        keys: The keys to access the array from factory.
+
+    """
+
+    def __init__(
+        self,
+        factory: "LazyFactory",
+        keys: Tuple[str, ...],
+    ) -> None:
+        self._total_count = factory._total_count
+        self._keys = keys
+        self._limit = factory._limit
+        self._pages: List[Union[LazyPage, pa.Array]] = [
+            LazyPage(pos, self) for pos in range(factory._page_number)
+        ]
+        self._factory = factory
+
+    def __len__(self) -> int:
+        return self._total_count
+
+    def __iter__(self) -> Iterator[Any]:
+        return chain(*self._pages)
+
+    def __getitem__(self, index: int) -> Any:
+        index = index if index >= 0 else self._total_count + index
+        return self._getitem(index)
+
+    def _getitem(self, index: int) -> Any:
+        div, mod = divmod(index, self._limit)
+        return self._pages[div][mod]
+
+
+class LazyPage:
+    """LazyPage is a placeholder of the pages in the lazy list when the page is not loaded yet.
+
+    Arguments:
+        pos: The page number.
+        parent: The parent lazy list.
+
+    """
+
+    def __init__(self, pos: int, parent: LazyList) -> None:
+        self._pos = pos
+        self._parent = parent
+
+    def _get_page(self) -> pa.Array:
+        # pylint: disable=protected-access
+        array = self._parent._factory.get_array(self._pos, self._parent._keys)
+        self._parent._pages[self._pos] = array
+        return array
+
+    def __getitem__(self, index: int) -> Any:
+        return self._get_page().__getitem__(index)
+
+    def __iter__(self) -> Iterator[Any]:
+        return self._get_page().__iter__()  # type: ignore[no-any-return]
+
+
+class LazyFactory:
+    """LazyFactory is a factory for creating lazy lists.
+
+    Arguments:
+        total_count: The total count of the elements in the lazy lists.
+        limit: The size of each lazy load page.
+        getter: A callable object to get the source data.
+
+    Examples:
+        >>> TOTAL_COUNT = 1000
+        >>> def getter(offset: int, limit: int) -> List[Dict[str, Any]]:
+        ...     stop = min(offset + limit, TOTAL_COUNT)
+        ...     return [
+        ...         {
+        ...             "remotePath": f"{i:06}.jpg",
+        ...             "label": {"CLASSIFICATION": {"category": "cat" if i % 2 else "dog"}},
+        ...         }
+        ...         for i in range(offset, stop)
+        ...     ]
+        ...
+        >>> factory = LazyFactory(TOTAL_COUNT, 128, getter)
+        >>> paths = factory.create_list(("remotePath",))
+        >>> categories = factory.create_list(("label", "CLASSIFICATION", "category"))
+        >>> len(paths)
+        1000
+        >>> list(paths)
+        [<pyarrow.StringScalar: '000000.jpg'>,
+         <pyarrow.StringScalar: '000001.jpg'>,
+         <pyarrow.StringScalar: '000002.jpg'>,
+         <pyarrow.StringScalar: '000003.jpg'>,
+         <pyarrow.StringScalar: '000004.jpg'>,
+         <pyarrow.StringScalar: '000005.jpg'>,
+         ...
+         ...
+         <pyarrow.StringScalar: '000999.jpg'>]
+        >>> len(categories)
+        1000
+        >>> list(categories)
+        [<pyarrow.StringScalar: 'dog'>,
+         <pyarrow.StringScalar: 'cat'>,
+         <pyarrow.StringScalar: 'dog'>,
+         <pyarrow.StringScalar: 'cat'>,
+         <pyarrow.StringScalar: 'dog'>,
+         ...
+         ...
+         <pyarrow.StringScalar: 'cat'>]
+
+    """
+
+    def __init__(self, total_count: int, limit: int, getter: Callable[[int, int], Any]) -> None:
+        self._getter = getter
+        self._total_count = total_count
+        self._limit = limit
+
+        page_number = ceil(total_count / limit)
+        self._pages: List[Optional[pa.StructArray]] = [None] * page_number
+        self._page_number = page_number
+
+    def get_array(self, pos: int, keys: Tuple[str, ...]) -> pa.Array:
+        """Get the array from the factory.
+
+        Arguments:
+            pos: The page number.
+            keys: The keys to access the array from factory.
+
+        Returns:
+            The requested pyarrow array.
+
+        """
+        array = self._pages[pos]
+        if array is None:
+            array = pa.array(self._getter(pos * self._limit, self._limit))
+            self._pages[pos] = array
+
+        for key in keys:
+            array = array.field(key)
+
+        return array
+
+    def create_list(self, keys: Tuple[str, ...]) -> LazyList:
+        """Create a lazy list from the factory.
+
+        Arguments:
+            keys: The keys to access the array from factory.
+
+        Returns:
+            A lazy list created by the given keys.
+
+        """
+        return LazyList(self, keys)
