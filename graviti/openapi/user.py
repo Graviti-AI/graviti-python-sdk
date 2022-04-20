e5045345cae9faf2112ce32d1a5c4048dd21c92d
changjun.zhu <changjun.zhu@graviti.com>
2022-04-20 14:48:02 +0800

    feat(openapi): adapt OpenAPI get_current_user
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/113

---
+++ graviti/openapi/user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Graviti. Licensed under MIT License.
+#
+
+"""Interfaces about the user."""
+
+from typing import Any, Dict
+from urllib.parse import urljoin
+
+from graviti.openapi.requests import open_api_do
+
+
+def get_current_user(access_key: str, url: str) -> Dict[str, Any]:
+    """Execute the OpenAPI `GET /v2/current-user`.
+
+    Arguments:
+        access_key: User's access key.
+        url: The URL of the graviti website.
+
+    Returns:
+        The response of OpenAPI.
+
+    Examples:
+        >>> get_current_user("ACCESSKEY-********", "https://gas.graviti.com/")
+        {
+            "id": "41438e9df9a82a194e1e76cc31c1d8d4",
+            "name": "czhual",
+            "nickname": "czh ual",
+            "email": "********@graviti.com",
+            "mobile": null,
+            "description": "",
+            "team": null
+        }
+
+    """
+    url = urljoin(url, "v2/current-user")
+    return open_api_do("GET", access_key, url).json()  # type: ignore[no-any-return]
