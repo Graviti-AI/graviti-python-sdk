300f17207f116a793123806891a200dbb29da98b
linji.xue <linji.xue@graviti.cn>
2022-05-03 20:50:02 +0800

    feat: adapt "FileSeries" with both "RemoteFile" and "File"
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/190

--- graviti/utility/file.py
+++ graviti/utility/file.py
@@ -7,18 +7,22 @@
 
 from hashlib import sha1
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, Type, TypeVar, Union
 
+import pyarrow as pa
 from _io import BufferedReader
 
 from graviti.utility.common import shorten
 from graviti.utility.repr import ReprMixin
 from graviti.utility.requests import UserResponse, config, get_session
 
+_T = TypeVar("_T", bound="File")
+
 
 class FileBase(ReprMixin):
     """This class represents the file in a DataFrame."""
 
+    _url: str
     _checksum: str
 
     def open(self) -> Union[UserResponse, BufferedReader]:
@@ -27,11 +31,18 @@ class FileBase(ReprMixin):
         Raises:
             NotImplementedError: The method of the base class should not be called.
 
-        Return:
-            The file pointer.
-
         """
         raise NotImplementedError
+
+    @property
+    def url(self) -> str:
+        """Get the url of the file.
+
+        Returns:
+            The url of the file.
+
+        """
+        return self._url
 
     @property
     def checksum(self) -> str:
@@ -43,6 +54,25 @@ class FileBase(ReprMixin):
         """
         return self._checksum
 
+    @staticmethod
+    def from_pyarrow(scalar: pa.StructScalar) -> Union["RemoteFile", "File"]:
+        """Create File or RemoteFile instance with pyarrow struct scalar.
+
+        Arguments:
+            scalar: The input pyarrow struct scalar.
+
+        Returns:
+            The File or RemoteFile instance.
+
+        """
+        url: str = scalar["url"].as_py()
+        checksum: str = scalar["checksum"].as_py()
+
+        if url.startswith("file://"):
+            return File._create(url, checksum)  # pylint: disable=protected-access
+
+        return RemoteFile(url, checksum)
+
 
 class RemoteFile(FileBase):
     """This class represents the file on Graviti platform.
@@ -53,18 +83,12 @@ class RemoteFile(FileBase):
 
     """
 
-    __slots__ = ("url", "_checksum")
-
     def __init__(self, url: str, checksum: str) -> None:
-        self.url = url
+        self._url = url
         self._checksum = checksum
 
     def _repr_head(self) -> str:
         return f'{self.__class__.__name__}("{shorten(self._checksum)}")'
-
-    def _urlopen(self) -> UserResponse:
-        session = get_session()
-        return UserResponse(session.request("GET", self.url, timeout=config.timeout, stream=True))
 
     def open(self) -> Union[UserResponse, BufferedReader]:
         """Return the binary file pointer of this file.
@@ -73,7 +97,8 @@ class RemoteFile(FileBase):
             The remote file pointer.
 
         """
-        return self._urlopen()
+        session = get_session()
+        return UserResponse(session.request("GET", self._url, timeout=config.timeout, stream=True))
 
 
 class File(FileBase):
@@ -87,10 +112,31 @@ class File(FileBase):
     _BUFFER_SIZE = 65536
 
     def __init__(self, path: str) -> None:
-        self.path = Path(path)
+        self.path = Path(path).absolute()
 
     def _repr_head(self) -> str:
         return f'{self.__class__.__name__}("{self.path.name}")'
+
+    @classmethod
+    def _create(cls: Type[_T], url: str, checksum: str) -> _T:
+        obj: _T = object.__new__(cls)
+        obj.path = Path(url[7:])
+        obj._url = url
+        obj._checksum = checksum
+        return obj
+
+    @property
+    def url(self) -> str:
+        """Get the url of the file.
+
+        Returns:
+            The url of the file.
+
+        """
+        if not hasattr(self, "_url"):
+            self._url = self.path.as_uri()
+
+        return self._url
 
     @property
     def checksum(self) -> str:
@@ -129,4 +175,4 @@ class File(FileBase):
             A python dict representation of the local file.
 
         """
-        return {"checksum": self.checksum}
+        return {"url": self.url, "checksum": self.checksum}
