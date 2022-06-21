6a4835c0c6681f39c60a13ee8303dd8049dd160f
yexuan.li <yexuan.li@graviti.com>
2022-06-21 16:11:20 +0800

    style(portex): move class "Fields" above class "ConnectedFields"
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/386

--- graviti/portex/field.py
+++ graviti/portex/field.py
@@ -106,7 +106,123 @@ class FrozenFields(FrozenNameOrderedDict[PortexType]):
         raise TypeError(f"Cannot rename '{old_name}' in {self.__class__.__name__}")
 
 
-UnionFields = Union[FrozenFields, "Fields"]
+class Fields(NameOrderedDict[PortexType], FrozenFields):  # type: ignore[misc]
+    """Represents a Portex ``record`` fields dict."""
+
+    def __init__(self, fields: Union[Iterable[Tuple[str, PortexType]], Mapping[str, PortexType]]):
+        super().__init__(fields)
+
+    @property
+    def imports(self) -> Imports:
+        """Get the Fields imports.
+
+        Returns:
+            The :class:`Imports` instance of this Fields.
+
+        """
+        imports = Imports()
+        for portex_type in self._data.values():
+            imports.update(portex_type.imports)
+
+        return imports
+
+    def insert(self, index: int, name: str, portex_type: PortexType) -> None:
+        """Insert the name and portex_type at the index.
+
+        Arguments:
+            index: The index to insert the field.
+            name: The name of the field to be inserted.
+            portex_type: The portex_type of the field to be inserted.
+
+        Raises:
+            KeyError: When the name already exists in the Fields.
+
+        """
+        self._check_key(name)
+        self._check_value(portex_type)
+
+        if self.__contains__(name):
+            raise KeyError(f'"{name}" already exists in the Fields')
+
+        self._keys.insert(index, name)
+        self._data.__setitem__(name, portex_type)
+
+    def astype(self, name: str, portex_type: PortexType) -> None:
+        """Convert the type of the field with the given name to the new PortexType.
+
+        Arguments:
+            name: The name of the field to convert.
+            portex_type: The new PortexType of the field to convert to.
+
+        Raises:
+            KeyError: When the name does not exist in the Fields.
+
+        """
+        self._check_value(portex_type)
+
+        if not self.__contains__(name):
+            raise KeyError(f'"{name}" does not exist in the Fields')
+
+        self._data.__setitem__(name, portex_type)
+
+    def rename(self, old_name: str, new_name: str) -> None:
+        """Rename the name of a field.
+
+        Arguments:
+            old_name: The current name of the field to be renamed.
+            new_name: The new name of the field to assign.
+
+        """
+        self._check_key(new_name)
+
+        self._data.__setitem__(new_name, self._data.pop(old_name))
+        self._keys.__setitem__(self._keys.index(old_name), new_name)
+
+    @classmethod
+    def from_pyobj(
+        cls, content: List[Dict[str, Any]], imports: Optional[Imports] = None
+    ) -> "Fields":
+        """Create Portex fields dict instance from python list.
+
+        Arguments:
+            content: A python list representing a Portex fields dict.
+            imports: The imports of the Portex fields dict.
+
+        Returns:
+            A Portex fields dict instance created from the input python list.
+
+        """
+        return Fields((item["name"], PortexType.from_pyobj(item, imports)) for item in content)
+
+    def to_pyobj(self) -> List[Dict[str, Any]]:
+        """Dump the instance to a python list.
+
+        Returns:
+            A Python List representation of the fields dict.
+
+        """
+        return [{"name": name, **portex_type.to_pyobj(False)} for name, portex_type in self.items()]
+
+    def to_pyarrow(self) -> List[pa.Field]:
+        """Convert the fields to a list of PyArrow Field.
+
+        Returns:
+            A list of PyArrow Field representing the fields of Portex record.
+
+        """
+        return [pa.field(name, portex_type.to_pyarrow()) for name, portex_type in self.items()]
+
+    def copy(self: _T) -> _T:
+        """Get a copy of the fields.
+
+        Returns:
+            A copy of the fields.
+
+        """
+        return self.__class__((name, portex_type.copy()) for name, portex_type in self.items())
+
+
+UnionFields = Union[FrozenFields, Fields]
 
 
 class ConnectedFields(MutableMapping[str, PortexType]):
@@ -240,119 +356,3 @@ class ConnectedFields(MutableMapping[str, PortexType]):
 
         """
         self._get_fields(old_name).rename(old_name, new_name)
-
-
-class Fields(NameOrderedDict[PortexType], FrozenFields):  # type: ignore[misc]
-    """Represents a Portex ``record`` fields dict."""
-
-    def __init__(self, fields: Union[Iterable[Tuple[str, PortexType]], Mapping[str, PortexType]]):
-        super().__init__(fields)
-
-    @property
-    def imports(self) -> Imports:
-        """Get the Fields imports.
-
-        Returns:
-            The :class:`Imports` instance of this Fields.
-
-        """
-        imports = Imports()
-        for portex_type in self._data.values():
-            imports.update(portex_type.imports)
-
-        return imports
-
-    def insert(self, index: int, name: str, portex_type: PortexType) -> None:
-        """Insert the name and portex_type at the index.
-
-        Arguments:
-            index: The index to insert the field.
-            name: The name of the field to be inserted.
-            portex_type: The portex_type of the field to be inserted.
-
-        Raises:
-            KeyError: When the name already exists in the Fields.
-
-        """
-        self._check_key(name)
-        self._check_value(portex_type)
-
-        if self.__contains__(name):
-            raise KeyError(f'"{name}" already exists in the Fields')
-
-        self._keys.insert(index, name)
-        self._data.__setitem__(name, portex_type)
-
-    def astype(self, name: str, portex_type: PortexType) -> None:
-        """Convert the type of the field with the given name to the new PortexType.
-
-        Arguments:
-            name: The name of the field to convert.
-            portex_type: The new PortexType of the field to convert to.
-
-        Raises:
-            KeyError: When the name does not exist in the Fields.
-
-        """
-        self._check_value(portex_type)
-
-        if not self.__contains__(name):
-            raise KeyError(f'"{name}" does not exist in the Fields')
-
-        self._data.__setitem__(name, portex_type)
-
-    def rename(self, old_name: str, new_name: str) -> None:
-        """Rename the name of a field.
-
-        Arguments:
-            old_name: The current name of the field to be renamed.
-            new_name: The new name of the field to assign.
-
-        """
-        self._check_key(new_name)
-
-        self._data.__setitem__(new_name, self._data.pop(old_name))
-        self._keys.__setitem__(self._keys.index(old_name), new_name)
-
-    @classmethod
-    def from_pyobj(
-        cls, content: List[Dict[str, Any]], imports: Optional[Imports] = None
-    ) -> "Fields":
-        """Create Portex fields dict instance from python list.
-
-        Arguments:
-            content: A python list representing a Portex fields dict.
-            imports: The imports of the Portex fields dict.
-
-        Returns:
-            A Portex fields dict instance created from the input python list.
-
-        """
-        return Fields((item["name"], PortexType.from_pyobj(item, imports)) for item in content)
-
-    def to_pyobj(self) -> List[Dict[str, Any]]:
-        """Dump the instance to a python list.
-
-        Returns:
-            A Python List representation of the fields dict.
-
-        """
-        return [{"name": name, **portex_type.to_pyobj(False)} for name, portex_type in self.items()]
-
-    def to_pyarrow(self) -> List[pa.Field]:
-        """Convert the fields to a list of PyArrow Field.
-
-        Returns:
-            A list of PyArrow Field representing the fields of Portex record.
-
-        """
-        return [pa.field(name, portex_type.to_pyarrow()) for name, portex_type in self.items()]
-
-    def copy(self: _T) -> _T:
-        """Get a copy of the fields.
-
-        Returns:
-            A copy of the fields.
-
-        """
-        return self.__class__((name, portex_type.copy()) for name, portex_type in self.items())
