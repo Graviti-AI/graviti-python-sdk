d42fd104eb4ff68199cfe46768807ef079703b32
changjun.zhu <changjun.zhu@graviti.com>
2022-06-22 18:36:23 +0800

    refactor(manager): remove the reset _data operation in "Draft.upload"
    Reset DataFrame.operations instead of Draft._data:
    - reduce redundant requests like "Get Sheet", "List Data"
    - ensure that the DataFrame that the user get before "upload" can still be used
    
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/395

--- graviti/manager/draft.py
+++ graviti/manager/draft.py
@@ -287,8 +287,7 @@ class Draft(Sheets):  # pylint: disable=too-many-instance-attributes
                     sheet=sheet_name,
                     jobs=jobs,
                 )
-
-        delattr(self, "_data")
+                dataframe.operations = []
 
 
 class DraftManager:
