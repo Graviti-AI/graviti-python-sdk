f686a52d62e12827e688c75bb8b25e94f88eb540
linji.xue <linji.xue@graviti.cn>
2022-11-10 15:43:57 +0800

    refactor: expose binary file classes in "graviti/__init__.py"
    PR Closed: https://github.com/Graviti-AI/graviti-python-sdk/pull/659

--- graviti/__init__.py
+++ graviti/__init__.py
@@ -8,7 +8,18 @@
 from graviti.__version__ import __version__
 from graviti.dataframe import ColumnSeries as Series
 from graviti.dataframe import DataFrame
+from graviti.file import Audio, File, Image, PointCloud
 from graviti.manager import Workspace
 from graviti.utility import engine
 
-__all__ = ["DataFrame", "Series", "Workspace", "__version__", "engine"]
+__all__ = [
+    "Audio",
+    "DataFrame",
+    "File",
+    "Image",
+    "PointCloud",
+    "Series",
+    "Workspace",
+    "__version__",
+    "engine",
+]
